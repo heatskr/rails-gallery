5cf7a0f2a80a17998ced79c77d805e3734ac8cdf
Giovanni Oliveira <giovanni.oliveira@gmail.com>
2014-04-05 21:26:27 -0300

    rails new gallery -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gallery::Application.config.secret_key_base = 'bd17f0e81d547a1ef4d21ffb5bdfe56ce31e092c5d8d93b60ae7308ca8633d68cbe0747a38675ae6ea284de64d287641a99f2198df9ed7badad6821cc38850a8'
